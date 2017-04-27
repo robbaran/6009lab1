71481d45a00914b7023f47eacd01be05bffed006
rob <robbaranowski@gmail.com>
2017-04-27 09:44:01 -0500

    renamed y to row

--- lab.py
+++ lab.py
@@ -7,16 +7,16 @@ def ncols(image):
 def nrows(image):
     return image["height"]
 
-def get_pixel(image, x, y, default=0):
-  index = x + ncols(image)*y
-  if x >= 0 and x < ncols(image) and y >= 0 and y < nrows(image): 
+def get_pixel(image, col, row, default=0):
+  index = col + ncols(image)*row
+  if col >= 0 and col < ncols(image) and row >= 0 and row < nrows(image): 
     pxl = image["pixels"][index]
   else:
     pxl = default
   return pxl  
 
-def set_pixel(image, x, y, color):
-  index = x + ncols(image)*y
+def set_pixel(image, col, row, color):
+  index = col + ncols(image)*row
   image["pixels"][index] = color
 
 def make_image(ref_image):
@@ -24,20 +24,20 @@ def make_image(ref_image):
 
 def legalize_range(image):
   result = make_image(image)
-  for x in range(ncols(image)):
-    for y in range(nrows(image)):
-      pxl = int(round(get_pixel(image, x, y)))
+  for col in range(ncols(image)):
+    for row in range(nrows(image)):
+      pxl = int(round(get_pixel(image, col, row)))
       pxl = min(255,pxl) #clip pxl at 255 max
-      set_pixel(result,x,y,pxl)
+      set_pixel(result,col,row,pxl)
   return result
 
 # return a new image by applying function f to each pixel of the input image
 def apply_per_pixel(image, f):
     result = make_image(image)
-    for x in range(ncols(result)):
-        for y in range(nrows(result)):
-            color = get_pixel(image, x, y)
-            set_pixel(result, x, y, f(color))
+    for col in range(ncols(result)):
+        for row in range(nrows(result)):
+            color = get_pixel(image, col, row)
+            set_pixel(result, col, row, f(color))
     return result
   
 def invert(c):
@@ -48,15 +48,15 @@ def convolve2d(image, kernel):
   #returns convolution of image with kernel
   result = make_image(image)	#start with empty image
   #iterate over every pixel in image
-  for x in range(ncols(image)):
-    for y in range(nrows(image)):
+  for col in range(ncols(image)):
+    for row in range(nrows(image)):
       conv = 0
       for kx in range(len(kernel[0])):
         for ky in range(len(kernel)):
-          pxl = get_pixel(image, x-1+kx, y-1+ky)
+          pxl = get_pixel(image, col-1+kx, row-1+ky)
           inc = pxl * kernel[ky][kx]
           conv += inc
-      set_pixel(result, x,y,conv)
+      set_pixel(result, col,row,conv)
   return result    
 
 def combine_images(image1, image2, f):
@@ -64,9 +64,9 @@ def combine_images(image1, image2, f):
   w = ncols(image1)
   h = nrows(image1)
   result = make_image(image1)
-  for x in range(w):
-    for y in range(h):
-      set_pixel(result, x, y, f(get_pixel(image1, x, y),get_pixel(image2, x, y)))
+  for col in range(w):
+    for row in range(h):
+      set_pixel(result, col, row, f(get_pixel(image1, col, row),get_pixel(image2, col, row)))
   return result
 
 def combine_pyth(pixel1,pixel2):
